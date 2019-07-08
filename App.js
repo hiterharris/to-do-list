b83fae2f039df0f0e0cf31ca2020e705c5b8361c
Hiter Harris <hiter.harris@levvel.io>
2019-07-08 10:09:31 -0400

    added text input

--- App.js
+++ App.js
@@ -23,7 +23,5 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
   },
 });
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -3,23 +3,53 @@ import {
     StyleSheet,
     Text,
     View,
+    StatusBar,
 } from 'react-native';
+import { LinearGradient } from 'expo';
+import { primaryGradientArray } from '../assets/Colors';
+import { gradientStart, gradientEnd } from '../assets/Colors';
 import Header from '../components/Header';
+import Input from '../components/Input';
 
 const headerTitle = "To Do List";
 
 export default class Main extends Component {
+    state = {
+        inputValue: ''
+    };
+    newInputValue = value => {
+        this.setState({
+            inputValue: value
+        });
+    };
     render() {
+        const { inputValue } = this.state;
         return (
-            <View style={styles.container}>
-                <Header title={headerTitle} />
-            </View>
+            <LinearGradient
+                colors={primaryGradientArray}
+                style={styles.container}
+            >
+                <StatusBar barStyle="light-content" />
+                <View style={styles.header}>
+                    <Header title={headerTitle} />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Input inputValue={inputValue} onChangeText={this.newInputValue} />
+                </View>
+            </LinearGradient>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1
+    },
+    header: {
         alignItems: 'center',
     },
+    inputContainer: {
+        marginTop: 40,
+        paddingLeft: 15
+    }
 });
