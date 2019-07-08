b2cc9437e442123d149336bcc43b2ef1df73606c
Hiter Harris <hiter.harris@levvel.io>
2019-07-08 11:34:03 -0400

    set up List component, adding it to Main component

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -4,23 +4,44 @@ import {
     Text,
     View,
     StatusBar,
+    ScrollView,
+    AsyncStorage,
 } from 'react-native';
 import { LinearGradient } from 'expo';
 import { primaryGradientArray } from '../assets/Colors';
 import { gradientStart, gradientEnd } from '../assets/Colors';
+import uuid from 'uuid/v1';
 import Header from '../components/Header';
 import Input from '../components/Input';
+import List from '../components/List';
 
 const headerTitle = "To Do List";
 
 export default class Main extends Component {
     state = {
-        inputValue: ''
+        inputValue: '',
+        loadingItems: false,
+        allItems: {},
+        isCompleted: false,
+    };
+    componentDidMount = () => {
+        this.loadingItems();
     };
     newInputValue = value => {
         this.setState({
-            inputValue: value
+            inputValue: value,
         });
+    };
+    loadingItems = async () => {
+        try {
+            const allItems = await AsyncStorage.getItem('ToDos');
+            this.setState({
+                loadingItems: true,
+                allItems: JSON.parse(allItems) || {},
+            });
+        } catch (err) {
+            console.log(err);
+        }
     };
     render() {
         const { inputValue } = this.state;
@@ -35,6 +56,22 @@ export default class Main extends Component {
                 </View>
                 <View style={styles.inputContainer}>
                     <Input inputValue={inputValue} onChangeText={this.newInputValue} />
+                </View>
+                <View style={styles.list}>
+                    <ScrollView contentContainerStyle={styles.scrollableList}>
+                        {Object.values(allItems)
+                            .reverse()
+                            .map(item => (
+                                <List
+                                    key={item.id}
+                                    {...item}
+                                    deleteItem={this.deleteItem}
+                                    completeItem={this.completeItem}
+                                    incompleteItem={this.incompleteItem}
+                                />
+                            ))
+                        }
+                    </ScrollView>
                 </View>
             </LinearGradient>
         );
@@ -51,5 +88,14 @@ const styles = StyleSheet.create({
     inputContainer: {
         marginTop: 40,
         paddingLeft: 15
-    }
+    },
+    list: {
+        flex: 1,
+        marginTop: 70,
+        paddingLeft: 15,
+        marginBottom: 10
+    },
+    scrollableList: {
+        marginTop: 15
+    },
 });
